edd968981c04fd7bf42dc5d080896ac229d24840
KetolaC <78930559+KetolaC@users.noreply.github.com>
2021-03-07 01:23:01 -0500

    Update adc_driver.vhd
    adc with updated logic

--- Test_VHDL_Entities/adc_driver.vhd
+++ Test_VHDL_Entities/adc_driver.vhd
@@ -10,7 +10,7 @@
 -- Tool Versions: 
 -- Description: Converts analog signals to digital
 -- 
--- Dependencies: i2c master
+-- Dependencies: i2c_master
 -- 
 -- Revision:
 -- Revision 0.01 - File Created
@@ -26,11 +26,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
 
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 ------------
 ---Entity---
 ------------
@@ -43,7 +38,6 @@ entity adc_driver is
            adc_sda      : inout std_logic;  --the i2c data
            adc_scl      : inout std_logic;  --the i2c clock
            adc_data_out : out std_logic_vector(7 downto 0) --the digitized data
-          -- adc_busy         : out std_logic  --for comminicating with i2c --probably not needed
     );
 end adc_driver;
 
@@ -60,9 +54,10 @@ architecture Behavioral of adc_driver is
 type state_type is  (init, standby, sample, instruct);
 signal state : state_type := init;
 signal m_ena, m_rw, m_busy, m_ack_error : std_logic;
+signal busy_prev    : std_logic;    --used to detect a change in the i2c busy state
 signal analog_in : std_logic_vector(7 downto 0); --The data sampled
 signal instructions : std_logic_vector(7 downto 0) := "000000" & adc_source; --The data for channel selection instructions
-signal adc_addr  : std_logic_vector(7 downto 0) := "1001000"; --used to select this device
+signal adc_addr  : std_logic_vector(6 downto 0) := "1001000"; --used to select this device
 
 ----------------
 ---Components---
@@ -70,8 +65,8 @@ signal adc_addr  : std_logic_vector(7 downto 0) := "1001000"; --used to select t
 
 component i2c_master is
   GENERIC(
-    input_clk : INTEGER := 50_000_000; --input clock speed from user logic in Hz
-    bus_clk   : INTEGER := 400_000);   --speed the i2c bus (scl) will run at in Hz
+    input_clk : INTEGER := 125_000_000; --input clock speed from user logic in Hz
+    bus_clk   : INTEGER := 100_000);   --speed the i2c bus (scl) will run at in Hz
   PORT(
     clk       : IN     STD_LOGIC;                    --system clock
     reset_n   : IN     STD_LOGIC;                    --active low reset
@@ -85,26 +80,33 @@ component i2c_master is
     sda       : INOUT  STD_LOGIC;                    --serial data output of i2c bus
     scl       : INOUT  STD_LOGIC);                   --serial clock output of i2c bus
 end component i2c_master;
-    
--- make component for ADC/DAC Register 
 
---component for flip flop??
-    
+--   component ila_0_0 is
+--     port(
+--     clk : in std_logic;
+--     probe0 : in std_logic_vector(7 downto 0);
+--     probe1 : in std_logic_vector(7 downto 0);
+--     probe2 : in std_logic;
+--     probe3 : in std_logic_vector(1 downto 0)
+--     );
+  
+--   end component;
+       
 begin
 
-------------------
----Instantiation--
-------------------
+-------------------
+---Instantiation---
+-------------------
 
 inst_i2c : i2c_master
   generic map(
-    input_clk => 50_000_000, --input clock speed from user logic in Hz
-    bus_clk   => 400_000)   --speed the i2c bus (scl) will run at in Hz !!NOTE THIS NEEDS TO BE CHANGED!!
+    input_clk => 125_000_000, --input clock speed from user logic in Hz
+    bus_clk   => 100_000)   --speed the i2c bus (scl) will run at in Hz
   port map(
     clk       =>    adc_clk,                    --system clock
     reset_n   =>    adc_rst,                    --active low reset
     ena       =>    m_ena,                      --latch in command
-    addr      =>    adc_addr(6 downto 0),       --address of target slave
+    addr      =>    adc_addr,                   --address of target slave
     rw        =>    m_rw,                       --'0' is write, '1' is read
     data_wr   =>    instructions,               --data to write to slave
     busy      =>    m_busy,                     --indicates transaction in progress
@@ -112,6 +114,15 @@ inst_i2c : i2c_master
     ack_error =>    m_ack_error,                --flag if improper acknowledge from slave
     sda       =>    adc_sda,                    --serial data output of i2c bus
     scl       =>    adc_scl);                   --serial clock output of i2c bus
+    
+--ILA : ila_0_0
+--   port map(
+--     clk => adc_clk,
+--     probe0 => analog_in,
+--     probe1 => instructions,
+--     probe2 => m_ena,
+--     probe3 => adc_source
+--   );
 
 ---------------
 ---Processes---
@@ -121,7 +132,7 @@ inst_i2c : i2c_master
 --State Machine---
 ------------------
 
-process(adc_clk, adc_rst)
+process(adc_clk, adc_rst, adc_en)
   begin
 
     if(adc_rst = '0' or adc_en = '0') then              --asynchronous reset
@@ -132,22 +143,22 @@ process(adc_clk, adc_rst)
            case state is
                 when init => state <= standby;  --resets and starts up the system
                              
-                when standby =>     if(m_busy = '1') then                               --Waits until slave is signaled for
+                when standby =>     if(m_busy = '1') then                               --waits until slave is signaled for
                                         state <= standby;
                                     else 
-                                        state <= instruct;                               --Goes to instruction mode when not busy
+                                        state <= instruct;                               --goes to instruction mode when not busy
                                     end if;
                                 
                 when sample =>      if(instructions(1 downto 0) = adc_source) then      --will sample as long as the channel is selected for
                                         state <= sample;
                                     else
-                                        state <= standby;                               --Goes back to standby mode when channel is changed
+                                        state <= standby;                               --goes back to standby mode when channel is changed
                                     end if;
                                
-                when instruct =>    if(m_busy'event and m_busy = '1') then              --only samples when engaged with the master
+                when instruct =>    if(m_busy = '0' and busy_prev = '1') then           --only samples after aknowledgeent form the master
                                         state <= sample;
                                     else
-                                        state <= instruct;                              --Waits for instructions
+                                        state <= instruct;                              --waits for instructions
                                     end if;
                                         
             end case;
@@ -160,16 +171,19 @@ end process;
 ---State Behaviour---
 ---------------------
        
-process(adc_clk, adc_rst, adc_en)
+process(adc_clk)
   begin
        
     if(rising_edge(adc_clk)) then 
     
+       instructions <= "000000" & adc_source;  --updates the instructions with the channel source
+       
        if(state = init) then
           m_ena <= '0';
           analog_in <= "00000000";
           m_busy <= '1'; 
           m_rw <= '1';  
+          --consider adding in a counter for timing purposes 
                              
        elsif(state = standby) then 
           m_ena <= '0';
@@ -182,13 +196,23 @@ process(adc_clk, adc_rst, adc_en)
        elsif(state = instruct) then
           m_ena <= '1'; --enables the I2C connection
           m_rw <= '0';  --changes to write mode
-          instructions <= "000000" and adc_source;  --updates the instructions with the channel source
           m_busy <= '1';      
        
        else 
-          state <= standby; --Just incase something weird happens, it'll stay in standby mode
+          state <= standby; --just incase something weird happens, it'll stay in standby mode
           
        end if;         
+    end if;
+end process;
+
+------------------------
+---Busy Signal Change---
+------------------------
+
+process(adc_clk)
+begin
+    if(rising_edge(adc_clk)) then 
+        busy_prev <= m_busy;
     end if;
 end process;
 
