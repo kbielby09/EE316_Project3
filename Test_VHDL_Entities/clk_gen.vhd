2c1ba08a8c5d17fb567f01fcbc83b93e842d0c38
kentcj <47125170+kentcj@users.noreply.github.com>
2021-03-08 18:10:29 -0500

    Update clk_gen.vhd

--- Test_VHDL_Entities/clk_gen.vhd
+++ Test_VHDL_Entities/clk_gen.vhd
@@ -2,7 +2,7 @@
 -- Filename: clk_gen.vhd
 -- Author: Chandler Kent
 -- Date Created: 2/28/21
--- Last Modified: 3/4/21
+-- Last Modified: 3/8/21
 -- Description: Clock generation code
 ----------------------------------------------------------------------------------
 
@@ -12,7 +12,7 @@ use ieee.numeric_std.all;
 
 entity clk_gen is
   port (
-    I_CLK_50MHz : in  std_logic;
+    I_CLK_125MHz : in std_logic;
     I_RESET_N   : in std_logic;
     Data_in     : in std_logic_vector(7 downto 0);
     Clock_out   : out std_logic
@@ -21,23 +21,32 @@ end clk_gen;
 
 architecture archclk_gen of clk_gen is
 
-  signal eightBitCounter : unsigned(7 downto 0) := (others => '0');
-  signal cnt_out : unsigned(7 downto 0) := (others => '0');
-  signal cnt_in : unsigned(7 downto 0) := (others => '0');
-  signal add_out : unsigned(7 downto 0) := (others => '0');
-  signal reg_out : unsigned(7 downto 0) := (others => '0');
+  signal eightBitCounter : unsigned(15 downto 0) := (others => '0');
+  signal cnt_out : unsigned(15 downto 0) := (others => '0');
+  signal cnt_in : unsigned(15 downto 0) := (others => '0');
+  signal add_out : unsigned(15 downto 0) := (others => '0');
+  signal reg_out : unsigned(15 downto 0) := (others => '0');
   signal comp_out : std_logic := '0';
-  signal mux1_out : unsigned(7 downto 0) := (others => '0');
-  signal mux2_out : unsigned(7 downto 0) := (others => '0');
+  signal mux1_out : unsigned(15 downto 0) := (others => '0');
+  signal mux2_out : unsigned(15 downto 0) := (others => '0');
+  signal new_data : unsigned(15 downto 0) := (others => '0');
+  signal tmp_a : unsigned(7 downto 0) := (others => '0');
+  signal tmp_b : unsigned(15 downto 0) := (others => '0');
   
 begin
-  cnt : process (I_CLK_50MHz, I_RESET_N)
+
+	--changing 0-255 to 500 - 1500
+	--tmp_a <= "11111111" - Data_in;
+	new_data <= ((256 - Data_in) * (1000 / 255)) + 500;  --idk if this is how you do this
+	
+	--change cnt_out
+    cnt : process(I_CLK_125MHz, I_RESET_N)
     begin
         if(I_RESET_N = '1') then 
-            cnt_out <= (others => '0');	--Not sure if this is correct syntax
+            cnt_out <= (others => '0');
     
-        elsif(rising_edge(I_CLK_50MHz)) then
---             if (std_logic_vector(eightBitCounter) <= Data_in) then
+        elsif(rising_edge(I_CLK_125MHz)) then
+             --if (std_logic_vector(eightBitCounter) <= Data_in) then
                 -- Clock_out <= '1';
                 -- --add counter to register value
 
@@ -47,40 +56,46 @@ begin
      end if;
   end process cnt;
   
-  add : process (Data_in) --need seperate process?
+  --changing mux1_out and mux2_out
+  add : process (I_CLK_125MHz) --need seperate process?
 	begin
-		if(I_RESET_N = '0') then
-			if(comp_out = '1') then 
-				mux2_out <= cnt_out + reg_out;
+		if (rising_edge(I_CLK_125MHz)) then
+			if(I_RESET_N = '0') then
+				if(comp_out = '1') then 
+					mux2_out <= cnt_out + reg_out;
+				else
+					mux1_out <= reg_out;
+				end if;
 			else
-				mux1_out <= reg_out;
+				mux2_out <= new_data;
 			end if;
-		else
-			mux2_out <= Data_in;
 		end if;
   end process add;
-	
-  comp : process ()
+
+--changing comp_out
+  comp : process (I_CLK_125MHz)
 	begin
-		if(cnt_out = reg_out) then
-			comp_out <= '1';
-			if(Clock_out = '0') then
-				Clock_out <= '1';
-			else 
-				Clock_out <= '0';
+		if (rising_edge(I_CLK_125MHz)) then
+			if(cnt_out = reg_out) then
+				comp_out <= '1';
+				--toggling Clock_out
+				if(Clock_out = '0') then
+					Clock_out <= '1';
+				else 
+					Clock_out <= '0';
+				end if;
+			else
+				comp_out <= '0';
 			end if;
-		else
-			comp_out <= '0';
 		end if;
   end process comp;
 
-  reg : process (I_CLK_50MHz)
+--changing reg_out
+  reg : process (I_CLK_125MHz)
 	begin
-		if rising_edge(I_CLK_50MHz) then
-			mux2_out <= reg_out;
+		if (rising_edge(I_CLK_125MHz)) then
+			reg_out <= mux2_out;
 		end if;
   end process reg;
   
-end architecture;
-
-
+end archclk_gen;
