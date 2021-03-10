b028362200bb4fde3b99279e5f096ae5de912ded
kbielby09 <kbielby09@gmail.com>
2021-03-09 21:01:25 -0500

    minor change to clock generation

--- Test_VHDL_Entities/clk_gen.vhd
+++ Test_VHDL_Entities/clk_gen.vhd
@@ -14,6 +14,7 @@ entity clk_gen is
   port (
     I_CLK_125MHz : in std_logic;
     I_RESET_N   : in std_logic;
+    On_off      : in std_logic;
     Data_in     : in std_logic_vector(7 downto 0) := X"4D";
     Clock_out   : out std_logic
   );
@@ -21,24 +22,11 @@ end clk_gen;
 
 architecture archclk_gen of clk_gen is
 
-  signal eightBitCounter : unsigned(15 downto 0) := (others => '0');
   signal cnt_out : unsigned(15 downto 0) := (others => '0');
-  signal cnt_in : unsigned(15 downto 0) := (others => '0');
-  signal add_out : unsigned(15 downto 0) := (others => '0');
-  signal reg_out : unsigned(15 downto 0) := (others => '0');
-  signal comp_out : std_logic := '0';
-  signal mux1_out : unsigned(15 downto 0) := (others => '0');
-  signal mux2_out : unsigned(15 downto 0) := (others => '0');
-  signal new_data : unsigned(15 downto 0) := (others => '0');
-  signal tmp_a : unsigned(7 downto 0) := (others => '0');
-  signal tmp_b : unsigned(15 downto 0) := (others => '0');
-
-  -- Signals created by Kyle
   signal counter : integer;
   signal increment : integer := 131; -- (delta 2 - delta 1) / 255
   signal delta_1 : integer := 50000; -- delta 1 constant: ( .5 * ( 1/1500 ) ) / sys_period
   signal clock_toggler : std_logic := '0';
-  signal test_input: std_logic_vector(7 downto 0) := X"FF";
 
 begin
 
@@ -46,12 +34,14 @@ begin
   KAB_COUNTER : process(I_CLK_125MHZ, I_RESET_N)
   begin
    if (rising_edge(I_CLK_125MHZ)) then
-      counter <= (delta_1 + increment * to_integer(unsigned(test_input))) / 2;  -- KAB get the value counted to
-      cnt_out <= cnt_out + 1;
-       if (cnt_out = to_unsigned(counter, 16)) then  -- KAB this comes from numeric_std library reference: nandland.com
-        clock_toggler <= not(clock_toggler);  -- toggle clock output
-        cnt_out <= (others => '0');
-       end if;
+     if (On_off = '1') then
+       counter <= (delta_1 + increment * to_integer(unsigned(Data_in))) / 2;  -- KAB get the value counted to
+       cnt_out <= cnt_out + 1;
+        if (cnt_out = to_unsigned(counter, 16)) then  -- KAB this comes from numeric_std library reference: nandland.com
+         clock_toggler <= not(clock_toggler);  -- toggle clock output
+         cnt_out <= (others => '0');
+        end if;
+     end if;
     end if;
   end process;
 
