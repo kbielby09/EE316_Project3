032027845c0ec83d6f79d5126e925156c4b61b5b
kbielby09 <kbielby09@gmail.com>
2021-03-10 19:16:47 -0500

    final top level entity

--- Test_VHDL_Entities/i2c_adc_top.vhd
+++ Test_VHDL_Entities/i2c_adc_top.vhd
@@ -35,12 +35,15 @@ entity i2c_adc_top is
  Port (
    I_CLK_125MHZ : in std_logic;
    I_RESET : in std_logic;
+   I_LCD : out std_logic_vector(1 downto 0);
 
    BTN1 : in std_logic;
+   PWM_OUT : out std_logic;
+   Clock_out   : out std_logic;
    LCD_SDA : inout std_logic;
-   LCD_SCL : inout std_logic
-   -- ADC_SDA : inout std_logic;
-   -- ADC_SCL : inout std_logic
+   LCD_SCL : inout std_logic;
+   ADC_SDA : inout std_logic;
+   ADC_SCL : inout std_logic
  );
 end i2c_adc_top;
 
@@ -58,6 +61,41 @@ architecture Behavioral of i2c_adc_top is
   );
   end component I2C_LCD_driver;
 
+  type control_state is (source1, source2, source3, source4, clockOff);
+  signal control_sig : control_state;
+
+  component kb_adc_driver is
+   Port (
+     I_CLK_125MHZ : in std_logic;
+     I_RESET : in std_logic;
+
+     source : in std_logic_vector(1 downto 0);
+     data_out : out std_logic_vector(7 downto 0); -- TODO implement for data output
+     data_ready : out std_logic;
+     ADC_SCL : inout std_logic;
+     ADC_SDA : inout std_logic
+   );
+ end component kb_adc_driver;
+
+ component pwm_gen is
+   Port (
+     I_CLK_50MHZ : in std_logic;
+     I_RESET  : in std_logic;
+     DATA_IN : in std_logic_vector(7 downto 0);
+     PWM_OUT : out std_logic
+   );
+ end component pwm_gen;
+
+ component clk_gen is
+   port (
+     I_CLK_125MHz : in std_logic;
+     I_RESET_N   : in std_logic;
+     On_off      : in std_logic;
+     Data_in     : in std_logic_vector(7 downto 0) := X"4D";
+     Clock_out   : out std_logic
+   );
+ end component clk_gen;
+
   type MODE is (PWM, CLOCK);
   signal generation_state : MODE := PWM;
 
@@ -66,6 +104,10 @@ architecture Behavioral of i2c_adc_top is
   signal pwm_enable : std_logic := '1';
   signal clock_enable : std_logic := '0';
   signal adc_source : std_logic_vector(1 downto 0) := "00";
+  signal adc_data : std_logic_vector(7 downto 0);
+  signal data_out : std_logic_vector(7 downto 0);
+  signal data_ready : std_logic;
+  signal previous_data_ready : std_logic;
 
 begin
 
@@ -74,19 +116,83 @@ begin
   I_RESET_N    => I_RESET,
   I_CLK_125MHZ => I_CLK_125MHZ,
   SDA          => LCD_SDA,
-  SCL          => LCD_SCL,  -- TODO add comma after created
+  SCL          => LCD_SCL,
   Generation   => clock_enable,
   source       => adc_source
   );
 
+  ADC : kb_adc_driver
+  port map(
+    I_CLK_125MHZ => I_CLK_125MHZ,
+    I_RESET => I_RESET,
+    source => adc_source,
+    data_out => data_out,
+    data_ready => data_ready,
+    ADC_SCL => ADC_SCL,
+    ADC_SDA => ADC_SDA
+  );
+
+  PWM_INST : pwm_gen
+  port map(
+    I_CLK_50MHZ => I_CLK_125MHZ,
+    I_RESET => I_RESET,
+    DATA_IN => adc_data,
+    PWM_OUT => PWM_OUT
+  );
+
+  CLOCK_INST : clk_gen
+  port map(
+    I_CLK_125MHz => I_CLK_125MHZ,
+    I_RESET_N    => I_RESET,
+    On_off       => clock_enable,
+    Data_in      => adc_data,
+    Clock_out    => Clock_out
+  );
+
+  REG_DATA : process(I_CLK_125MHZ, I_RESET)
+  begin
+    if (rising_edge(I_CLK_125MHZ)) then
+      previous_data_ready <= data_ready;
+      if (previous_data_ready = '0' and data_ready = '1') then
+        adc_data <= data_out;
+      end if;
+    end if;
+  end process;
+
+  CLOCK_TOGGLE : process(I_CLK_125MHZ, I_RESET)
+  begin
+    if (rising_edge(I_CLK_125MHZ)) then
+      if (previous_button1_value = '0' and BTN1 = '1') then
+        clock_enable <= not(clock_enable);
+      end if;
+    end if;
+  end process;
+
+  BTN_PRESS : process(I_CLK_125MHZ)
+  begin
+    if (rising_edge(I_CLK_125MHZ)) then
+      previous_button1_value <= BTN1;
+    end if;
+  end process;
+
   SOURCE_SEL : process(I_CLK_125MHZ, I_RESET)
   begin
-    if (I_RESET = '1') then
-    elsif (rising_edge(I_CLK_125MHZ)) then
-      previous_button1_value <= BTN1;
+    if (rising_edge(I_CLK_125MHZ)) then
+
       case(generation_state) is
         when PWM =>
           if (previous_button1_value = '0' and BTN1 = '1') then
+            -- case( control_sig ) is
+            --
+            --   when source1 =>
+            --     control_sig <= source2;
+            --     adc_source <= "01";
+            --   when IDLE =>
+            --   when IDLE =>
+            --   when IDLE =>
+            --   when IDLE =>
+
+            -- end case;
             case(adc_source) is
               when "00" =>
                 adc_source <= "01";  -- Change input to LDR
@@ -98,12 +204,16 @@ begin
                 adc_source <= "00"; -- Change to LDR
             end case;
           end if;
+          -- generation_state <= PWM;
         when CLOCK =>
-          if (previous_button1_value = '0' and BTN1 = '1') then
-            clock_enable <= not(clock_enable);
-          end if;
+          -- if (previous_button1_value = '0' and BTN1 = '1') then
+          --   clock_enable <= not(clock_enable);
+          -- end if;
+          generation_state <= PWM; -- TODO remove after
       end case;
     end if;
   end process;
+
+  I_LCD <= adc_source;
 
 end Behavioral;
