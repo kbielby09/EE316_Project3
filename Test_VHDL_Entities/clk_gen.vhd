4424d7dd35b476d07cb67d2ffc62f0b54ac3fae6
kentcj <47125170+kentcj@users.noreply.github.com>
2021-03-10 21:50:20 -0500

    Update clk_gen.vhd

--- Test_VHDL_Entities/clk_gen.vhd
+++ Test_VHDL_Entities/clk_gen.vhd
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------------
 -- Filename: clk_gen.vhd
--- Author: Chandler Kent
+-- Authors: Chandler Kent and Kyle Bielby
 -- Date Created: 2/28/21
--- Last Modified: 3/8/21
+-- Last Modified: 3/10/21
 -- Description: Clock generation code
 ----------------------------------------------------------------------------------
 
@@ -12,21 +12,21 @@ use ieee.numeric_std.all;
 
 entity clk_gen is
   port (
-    I_CLK_125MHz : in std_logic;
-    I_RESET_N   : in std_logic;
-    On_off      : in std_logic;
-    Data_in     : in std_logic_vector(7 downto 0) := X"4D";
-    Clock_out   : out std_logic
+    I_CLK_125MHz 	: in std_logic;
+    I_RESET_N   	: in std_logic;
+    On_off      	: in std_logic;
+    Data_in     	: in std_logic_vector(7 downto 0) := X"4D";
+    Clock_out   	: out std_logic
   );
 end clk_gen;
 
 architecture archclk_gen of clk_gen is
 
-  signal cnt_out : unsigned(15 downto 0) := (others => '0');
-  signal counter : integer;
-  signal increment : integer := 131; -- (delta 2 - delta 1) / 255
-  signal delta_1 : integer := 50000; -- delta 1 constant: ( .5 * ( 1/1500 ) ) / sys_period
-  signal clock_toggler : std_logic := '0';
+  signal cnt_out 		: unsigned(19 downto 0) := (others => '0');
+  signal counter 		: integer;
+  signal increment 		: integer := 327; -- (delta 2 - delta 1) / 255
+  signal delta_1 		: integer := 125000; -- delta 1 constant: ( .5 * ( 1/1500 ) ) / sys_period
+  signal clock_toggler 	: std_logic := '0';
 
 begin
 
@@ -37,7 +37,7 @@ begin
      if (On_off = '1') then
        counter <= (delta_1 + increment * to_integer(unsigned(Data_in))) / 2;  -- KAB get the value counted to
        cnt_out <= cnt_out + 1;
-        if (cnt_out = to_unsigned(counter, 16)) then  -- KAB this comes from numeric_std library reference: nandland.com
+        if (cnt_out = to_unsigned(counter, 20)) then  -- KAB this comes from numeric_std library reference: nandland.com
          clock_toggler <= not(clock_toggler);  -- toggle clock output
          cnt_out <= (others => '0');
         end if;
